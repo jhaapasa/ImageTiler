01bbc24747b12354802172a2bc57c0f5034db466
Jaakko Haapasalo <jhaapasalo@nvidia.com>
2016-04-14 15:05:19 +0300

    Basic parameter parsing.

--- ImageTiler/Program.cs
+++ ImageTiler/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,55 @@ using System.Threading.Tasks;
 
 namespace ImageTiler
 {
+    public class Options
+    {
+        [Option('c', "columns", Required = false, DefaultValue = 4, HelpText = "Number of columns")]
+        public int Columns { get; set; }
+
+        [Option('w', "width", Required = false, DefaultValue = 1024, HelpText = "Width of tiled image in pixels")]
+        public int Width { get; set; }
+
+        [Option('o', "output", Required = false, DefaultValue = "tile.png", HelpText = "Output filename for tilemap (extension used for format selection")]
+        public string OutputFilename { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                var options = new Options();
+                ParseOptions(args, options);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+            }
         }
+
+        private static void ParseOptions(string[] args, Options options)
+        {
+            if (!CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                foreach (var s in options.LastParserState.Errors)
+                {
+                    Console.WriteLine(s.ToString());
+                }
+                Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+            }
+        }
+
     }
 }
